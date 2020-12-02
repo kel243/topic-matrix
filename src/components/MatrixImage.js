c1b86f8621f4f004e2a8a14f31c5b0c6cb04f76c
Ken Lin <kel117@pitt.edu>
2020-12-02 00:32:54 -0500

    implemented favoriting feature

--- src/components/MatrixImage.js
+++ src/components/MatrixImage.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const MatrixImage = ({ image, query }) => {
-  return <MatrixImg src={image.src.original} alt={query}></MatrixImg>;
+const MatrixImage = ({ image, video, query, favorites, setFavorites }) => {
+  const [favorited, setFavorited] = useState(false);
+  const imgRef = useRef(null);
+
+  // Add styling to favorited images/videos
+  useEffect(() => {
+    if (favorited) {
+      imgRef.current.classList.add("favorited");
+    } else {
+      imgRef.current.classList.remove("favorited");
+    }
+  }, [favorited]);
+
+  // Toggle favorited state
+  // Add image/video to favorites state if it is favorited
+  // Remove image/video from favorites state if it is unfavorited
+  const clickHandler = (mode) => {
+    setFavorited(!favorited);
+    console.log("ran");
+    if (mode === "photo") {
+      if (favorited) {
+        setFavorites([...favorites, image.url]);
+      } else {
+        setFavorites(favorites.filter((favorite) => favorite !== image.url));
+      }
+    } else {
+      if (favorited) {
+        setFavorites([...favorites, video.url]);
+      } else {
+        setFavorites(favorites.filter((favorite) => favorite !== video.url));
+      }
+    }
+  };
+
+  if (image) {
+    return (
+      <MatrixImg
+        ref={imgRef}
+        src={image.src.original}
+        alt={query}
+        onClick={() => clickHandler("photo")}
+      ></MatrixImg>
+    );
+  } else if (video) {
+    return (
+      <MatrixImg
+        ref={imgRef}
+        src={video.image}
+        alt={query}
+        onClick={() => clickHandler("video")}
+      ></MatrixImg>
+    );
+  }
 };
 
 const MatrixImg = styled.img`
@@ -10,6 +61,8 @@ const MatrixImg = styled.img`
   height: 100%;
   cursor: pointer;
   position: relative;
+
+  transition: all 0.1s ease;
 `;
 
 export default MatrixImage;
