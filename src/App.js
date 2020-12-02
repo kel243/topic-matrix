2a50bb6a226242d42fcbfebdbe284a0bf4020a16
Ken Lin <kel117@pitt.edu>
2020-12-01 22:23:56 -0500

    initial cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Matrix from "./components/Matrix";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+  const [videos, setVideos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Matrix images={images} videos={videos} />
     </div>
   );
 }
